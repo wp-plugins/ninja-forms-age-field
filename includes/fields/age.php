6a70aa9bd6ef5be86f850e1de8270f2ad50c4f6e
ivan_paulin <ivan_paulin@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-06-17 19:57:09 +0000

    update version and add more text in readme
    git-svn-id: https://plugins.svn.wordpress.org/ninja-forms-age-field/trunk@1182859 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- includes/fields/age.php
+++ includes/fields/age.php
@@ -1,29 +1,39 @@
 <?php
+
 /**
  * Register new Ninja Forms field
  */
-function ninja_forms_register_field_age(){
+function ninja_forms_register_field_age()
+{
 	$args = array(
-		'name' => __( 'Age', 'ninja-forms' ),
-		'sidebar' => 'template_fields',
-		'edit_function' => 'ninja_forms_field_age_edit',
-		'display_function' => 'ninja_forms_field_age_display',
-		'save_function' => '',
-		'group' => 'standard_fields',
-		'edit_label' => true,
-		'edit_label_pos' => true,
-		'edit_req' => true,
-		'edit_custom_class' => true,
-		'edit_help' => true,
-		'edit_desc' => true,
-		'edit_meta' => false,
-		'edit_conditional' => true,
-		'conditional' => array(
-			'value' => array(
-				'type' => 'text',
+		'name'               =>  __( 'Age', 'ninja-forms' ),
+		'sidebar'            =>  'template_fields',
+		'edit_function'      =>  'ninja_forms_field_age_edit',
+		'display_function'   =>  'ninja_forms_field_age_display',
+		'save_function'      =>  '',
+		'group'              =>  'standard_fields',
+		'edit_options' => array(
+			array(
+				'type' => 'checkbox',
+				'name' => 'datepicker',
+				'label' => __( 'Datepicker', 'ninja-forms' ),
+				'default' => 1,
 			),
 		),
-		'pre_process' => 'ninja_forms_field_age_pre_process',
+		'edit_label'         =>  true,
+		'edit_label_pos'     =>  true,
+		'edit_req'           =>  true,
+		'edit_custom_class'  =>  true,
+		'edit_help'          =>  true,
+		'edit_desc'          =>  true,
+		'edit_meta'          =>  false,
+		'edit_conditional'   =>  true,
+		'conditional'        =>  array(
+			                        'value' => array(
+				                       'type' => 'text',
+			                        ),
+		                         ),
+		'pre_process'        =>  'ninja_forms_field_age_pre_process',
 	);
 
 	ninja_forms_register_field( '_age', $args );
@@ -35,7 +45,8 @@ add_action( 'init', 'ninja_forms_register_field_age' );
 /**
  * Edit field in admin
  */
-function ninja_forms_field_age_edit( $field_id, $data ){
+function ninja_forms_field_age_edit( $field_id, $data )
+{
 	$plugin_settings = nf_get_settings();
 	
 	$date_format = "m/d/Y";
@@ -43,12 +54,12 @@ function ninja_forms_field_age_edit( $field_id, $data ){
 	$custom = '';
 	
 	// Default Value
-	if( isset( $data['default_value'] ) ){
+	if( isset( $data['default_value'] ) ) {
 		$default_value = $data['default_value'];
-	}else{
+	} else {
 		$default_value = '';
 	}
-	if( $default_value == 'none' ){
+	if( $default_value == 'none' ) {
 		$default_value = '';
 	}
 
@@ -59,17 +70,17 @@ function ninja_forms_field_age_edit( $field_id, $data ){
 			<?php _e( 'Default Value' , 'ninja-forms'); ?>
 		</label><br />
 			<select id="default_value_<?php echo $field_id;?>" name="" class="widefat ninja-forms-_text-default-value">
-				<option value="" <?php if( $default_value == ''){ echo 'selected'; $custom = 'no';}?>><?php _e('None', 'ninja-forms'); ?></option>
-				<option value="_custom" <?php if($custom != 'no'){ echo 'selected';}?>><?php _e('Custom', 'ninja-forms'); ?> -></option>
+				<option value="" <?php if( $default_value == '') { echo 'selected'; $custom = 'no'; } ?>><?php _e( 'None', 'ninja-forms' ); ?></option>
+				<option value="_custom" <?php if($custom != 'no') { echo 'selected'; } ?>><?php _e( 'Custom', 'ninja-forms' ); ?> -></option>
 			</select>
 		</span>
 	</div>
 	<div class="description description-thin">
 
-		<label for="" id="default_value_label_<?php echo $field_id;?>" style="<?php if($custom == 'no'){ echo 'display:none;';}?>">
+		<label for="" id="default_value_label_<?php echo $field_id;?>" style="<?php if( $custom == 'no' ) { echo 'display:none;'; } ?>">
 			<span class="field-option">
-			<?php _e( 'Default Value' , 'ninja-forms'); ?><br />
-			<input type="text" class="widefat code" name="ninja_forms_field_<?php echo $field_id;?>[default_value]" id="ninja_forms_field_<?php echo $field_id;?>_default_value" value="<?php echo $default_value;?>" />
+			<?php _e( 'Default Value' , 'ninja-forms' ); ?><br />
+			<input type="text" class="widefat code" name="ninja_forms_field_<?php echo $field_id; ?>[default_value]" id="ninja_forms_field_<?php echo $field_id; ?>_default_value" value="<?php echo $default_value; ?>" />
 			</span>
 		</label>
 
@@ -82,39 +93,39 @@ function ninja_forms_field_age_edit( $field_id, $data ){
 /**
  * Display field on front-end
  */
-function ninja_forms_field_age_display( $field_id, $data ){
+function ninja_forms_field_age_display( $field_id, $data )
+{
 	global $current_user;
 	$field_class = ninja_forms_get_field_class( $field_id );
 
-
-	if(isset($data['default_value'])){
+	if( isset( $data['default_value'] ) ) {
 		$default_value = $data['default_value'];
-	}else{
+	} else {
 		$default_value = '';
 	}
 
-	if(isset($data['label_pos'])){
+	if( isset( $data['label_pos'] ) ) {
 		$label_pos = $data['label_pos'];
-	}else{
+	} else {
 		$label_pos = "left";
 	}
 
-	if(isset($data['label'])){
+	if( isset( $data['label'] ) ) {
 		$label = $data['label'];
-	}else{
+	} else {
 		$label = '';
 	}
 
-	if( isset( $data['mask'] ) ){
+	if( isset( $data['mask'] ) ) {
 		$mask = $data['mask'];
-	}else{
-		$mask = '';
+	} else {
+		$mask = 'mm/dd/yyy';
 	}	
 
 	$mask_class = 'ninja-forms-datepicker';
 	
 	?>
-	<input id="ninja_forms_field_<?php echo $field_id;?>" data-mask="<?php echo $mask; ?>"    name="ninja_forms_field_<?php echo $field_id;?>" type="text" class="<?php echo $field_class;?> <?php echo $mask_class;?>" value="<?php echo $default_value;?>" rel="<?php echo $field_id;?>" />
+	<input id="ninja_forms_field_<?php echo $field_id; ?>" data-mask="<?php echo $mask; ?>"   name="ninja_forms_field_<?php echo $field_id; ?>" type="text" class="<?php echo $field_class; ?> <?php echo $mask_class; ?> ninja-forms-date" value="<?php echo $default_value; ?>" rel="<?php echo $field_id; ?>" />
 	<?php
 
 }
@@ -126,10 +137,11 @@ function ninja_forms_field_age_display( $field_id, $data ){
  * @param int $field_id  Field ID
  * @param string $user_value  Field value
  */
-function ninja_forms_field_age_pre_process( $field_id, $user_value ){
+function ninja_forms_field_age_pre_process( $field_id, $user_value )
+{
 	global $ninja_forms_processing;
 		
-	$user_value = explode("/", $user_value);
+	$user_value = explode( "/", $user_value );
 
 	$age  = ( date( "md", date( "U", mktime( 0, 0, 0, $user_value[0], $user_value[1], $user_value[2] ) ) ) > date( "md" ) ? ( ( date( "Y" ) - $user_value[2] ) - 1 ) : ( date( "Y" ) - $user_value[2] ) );
 		
--- ninja-forms-age-field.php
+++ ninja-forms-age-field.php
@@ -3,7 +3,7 @@
  * Plugin Name: Ninja Forms Age Field
  * Plugin URI: http://wordpress.org/plugins/ninja-forms-age-field/
  * Description: This plugin uses date picker field on front-end for adding date of birth, and converts birth date into age number. Input date format must be: mm/dd/yyy
- * Version: 1.1
+ * Version: 1.1.1
  * Author: Ivan Paulin
  * Author URI: http://www.ivanpaulin.com
  * License:  GPLv2 or later
@@ -14,7 +14,7 @@
 define("NINJA_FORMS_AGE_FIELD_DIR", WP_PLUGIN_DIR."/".basename( dirname( __FILE__ ) ) );
  
 // Check if Ninja Forms plugin is activated
-if( in_array( 'ninja-forms/ninja-forms.php', apply_filters( 'active_plugins', get_option( 'active_plugins' ) ) ) ){
+if( in_array( 'ninja-forms/ninja-forms.php', apply_filters( 'active_plugins', get_option( 'active_plugins' ) ) ) ) {
 	// Load field file
 	require_once( NINJA_FORMS_AGE_FIELD_DIR . "/includes/fields/age.php" );
 
